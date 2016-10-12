243008f50a88a7cb872750369a6cc7218baec43e
pavellunin <pavel.lunin.pl@gmail.com>
2016-10-13 00:15:43 +0200

    Begonnen mit Farbslider der angibt wieviel Farbe der Player noch zur verfügung hat...

---
+++ Assets/Scripts/PlayerColorScript.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+
+public class PlayerColorScript : MonoBehaviour {
+    public int startingColor = 10;                            
+    public int currentColor;                                  
+    public Slider colorSlider;
+    public Transform player;
+                                                                // Use this for initialization
+    void Start () {
+
+       
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        Debug.Log(colorSlider.transform.position);
+        var wantedPos = Camera.main.WorldToViewportPoint(player.position);
+        transform.position = wantedPos;
+    }
+}
